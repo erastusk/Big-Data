a078b51c451b5db3cbb4396878ddf839fcaf846e
erastusk <kj5574@yahoo.com>
2018-01-05 14:30:47 -0600

    Add files via upload

---
+++ Geo Mapping/gps.sh
@@ -0,0 +1,800 @@
+curl -XPUT "http://kibana:9200/gps/gps/1" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": lat,
+    "lon": lon
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/2" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.180555,
+    "lon": -66.749961
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/3" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.361945,
+    "lon": -67.175597
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/4" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.455183,
+    "lon": -67.119887
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/5" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.158345,
+    "lon": -66.932911
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/6" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.295366,
+    "lon": -67.125135
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/7" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.402253,
+    "lon": -66.711397
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/8" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.420412,
+    "lon": -66.671979
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/9" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.445147,
+    "lon": -66.559696
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/10" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.991245,
+    "lon": -67.153993
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/11" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.083361,
+    "lon": -67.153897
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/12" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.064919,
+    "lon": -66.716683
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/13" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.4126,
+    "lon": -66.863926
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/14" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.190607,
+    "lon": -66.832041
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/15" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.076713,
+    "lon": -66.947389
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/16" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.295913,
+    "lon": -66.515588
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/17" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.263085,
+    "lon": -66.712985
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/18" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.43315,
+    "lon": -66.285875
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/19" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.963613,
+    "lon": -66.947127
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/20" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.349417,
+    "lon": -66.578078
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/21" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.448452,
+    "lon": -66.594127
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/22" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.985033,
+    "lon": -66.886536
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/23" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.053539,
+    "lon": -66.792931
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/24" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.407226,
+    "lon": -66.808999
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/25" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.134695,
+    "lon": -67.116199
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/26" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.46832,
+    "lon": -67.015781
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/27" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.21033,
+    "lon": -66.591616
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/28" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.003422,
+    "lon": -67.03581
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/29" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.277102,
+    "lon": -66.869645
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/30" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.240187,
+    "lon": -66.988776
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/31" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.422908,
+    "lon": -66.489337
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/32" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.377637,
+    "lon": -67.079574
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/33" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.332568,
+    "lon": -67.227022
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/34" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.434099,
+    "lon": -66.927384
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/35" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.178225,
+    "lon": -67.151514
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/36" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.221464,
+    "lon": -67.156039
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/37" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.1078,
+    "lon": -67.037263
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/38" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.332929,
+    "lon": -66.959689
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/39" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.319026,
+    "lon": -66.420557
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/40" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.382311,
+    "lon": -66.626416
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/41" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.49551,
+    "lon": -67.098671
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/42" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.410188,
+    "lon": -66.336556
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/43" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.422987,
+    "lon": -66.397214
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/44" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.482073,
+    "lon": -66.391274
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/45" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.064848,
+    "lon": -66.856319
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/46" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.248401,
+    "lon": -66.130662
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/47" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.96577,
+    "lon": -66.219555
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/48" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.128961,
+    "lon": -66.266683
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/49" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.014055,
+    "lon": -65.920751
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/50" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.003025,
+    "lon": -66.046237
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/51" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.011233,
+    "lon": -66.560065
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/52" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.991269,
+    "lon": -66.606774
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/53" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.003222,
+    "lon": -66.614037
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/54" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.231988,
+    "lon": -65.759623
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/55" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.289927,
+    "lon": -66.25344
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/56" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.217946,
+    "lon": -66.428076
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/57" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.043498,
+    "lon": -66.015479
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/58" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.218819,
+    "lon": -66.042375
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/59" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.215308,
+    "lon": -66.073565
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/60" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.989853,
+    "lon": -66.664116
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/61" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.323585,
+    "lon": -65.883206
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/62" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.030831,
+    "lon": -66.616838
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/63" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.109947,
+    "lon": -66.635622
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/64" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.252603,
+    "lon": -65.682494
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/65" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.103624,
+    "lon": -66.151667
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/66" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.308717,
+    "lon": -65.738915
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/67" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.177233,
+    "lon": -66.160645
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/68" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.331178,
+    "lon": -65.634104
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/69" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.163855,
+    "lon": -65.754042
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/70" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.352216,
+    "lon": -65.817387
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/71" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.001317,
+    "lon": -66.252183
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/72" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.147107,
+    "lon": -65.976167
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/73" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.995441,
+    "lon": -66.391334
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/74" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.130096,
+    "lon": -65.439369
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/75" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.133196,
+    "lon": -66.476916
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/76" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.069489,
+    "lon": -65.896161
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/77" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.1038,
+    "lon": -66.357586
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/78" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.187148,
+    "lon": -65.871189
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/79" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.438968,
+    "lon": -65.905168
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/80" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.342888,
+    "lon": -65.723109
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/81" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.326599,
+    "lon": -65.30772
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/82" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.224133,
+    "lon": -65.908542
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/83" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.264076,
+    "lon": -65.97851
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/84" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.102834,
+    "lon": -66.568105
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/85" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.225708,
+    "lon": -66.221633
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/86" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.30391,
+    "lon": -66.326179
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/87" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.010714,
+    "lon": -66.133615
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/88" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.155424,
+    "lon": -66.229907
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/89" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.136782,
+    "lon": -65.821476
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/90" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.198955,
+    "lon": -66.309833
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/91" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.060494,
+    "lon": -66.500624
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/92" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.349871,
+    "lon": -64.974016
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/93" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.340709,
+    "lon": -64.923329
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/94" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.7431,
+    "lon": -64.686556
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/95" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.723548,
+    "lon": -64.745932
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/96" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.744557,
+    "lon": -64.682933
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/97" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.338559,
+    "lon": -64.73653
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/98" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 18.327294,
+    "lon": -64.78672
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/99" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.678145,
+    "lon": -64.902355
+  }
+}'
+curl -XPUT "http://kibana:9200/gps/gps/100" -H 'Content-Type: application/json' -d'
+{
+   "text": "Geo-point as an object",
+   "location": {
+    "lat": 17.758634,
+    "lon": -64.817451
+  }
+}'
